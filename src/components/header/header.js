eeced4466f0029fa87e8765b4ca3c3a8b57a0804
George Pearson <george.pearson@dorsetsoftware.com>
2019-09-05 15:11:09 +0100

    Add font awesome

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,18 +2,33 @@ import React from "react";
 import logo from "./logo.svg";
 import * as style from './style.module.css';
 import { Link } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 export default class Header extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      navIconActive: false
+    }
+  }
+
+  toggleNavIcon = () => {
+    this.setState({
+      navIconActive: !this.state.navIconActive
+    });
+  }
+
   render() {
+    const navIcon = this.state.navIconActive ? faTimes : faBars;
     return (
       <header className={style.header}>
         <nav className={style.navBar}>
           <div className={style.navBarBrand}>
-            <img src={logo} class={style.logo} alt="logo" />
-            <div className={style.navBarBurger}>
-              <div className={style.navBarBurgerItem} />
-              <div className={style.navBarBurgerItem} />
-              <div className={style.navBarBurgerItem} />
+            <img src={logo} className={style.logo} alt="logo" />
+            <div className={style.navIconDiv} onClick={this.toggleNavIcon}>
+              <FontAwesomeIcon className={style.navIcon} icon={navIcon} size="2x"/>
             </div>
           </div>
           <div className={style.navBarMenu}>
