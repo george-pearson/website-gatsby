1b16fbb1ceb0defc3d2a36b5aafef7f0c0cc277d
George Pearson <george.pearson@dorsetsoftware.com>
2019-09-05 08:54:49 +0100

    basic navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import * as style from './style.module.css';
+import { Link } from 'gatsby';
 
 export default class Header extends React.Component {
   render() {
     return (
       <header className={style.header}>
-        <img src={logo} class={style.logo} alt="logo" />
+        <nav className={style.navBar}>
+          <div className={style.navBarBrand}>
+            <img src={logo} class={style.logo} alt="logo" />
+            <div className={style.navBarBurger}>
+              <span className={style.navBarBurgerItem} />
+              <span className={style.navBarBurgerItem} />
+              <span className={style.navBarBurgerItem} />
+            </div>
+          </div>
+          <div className={style.navBarMenu}>
+            <Link to="/" className={style.navBarItem}>Home</Link>
+            <Link to="/" className={style.navBarItem}>About</Link>
+            <Link to="/" className={style.navBarItem}>Photos</Link>
+          </div>
+        </nav>
+        
       </header>
     )
   }
