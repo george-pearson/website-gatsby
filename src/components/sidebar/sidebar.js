e4fb6788fa19e713e68cacd6b7aa2b79b3c21c6d
George Pearson <george.pearson@dorsetsoftware.com>
2019-09-06 13:48:00 +0100

    Add logo to sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as style from './style.module.css';
+import logo from "./logo.svg";
 import { Link } from 'gatsby';
 
 export default class Sidebar extends React.Component {
@@ -8,6 +9,7 @@ export default class Sidebar extends React.Component {
     const sidebarClass = this.props.show ? style.sidebarOpen : style.sidebar;
     return (
       <nav className={sidebarClass}>
+        <img src={logo} className={style.logo} alt="logo" />
         <Link to="/" className={style.sidebarItem}>Home</Link>
         <Link to="/" className={style.sidebarItem}>About</Link>
         <Link to="/" className={style.sidebarItem}>Photos</Link>
