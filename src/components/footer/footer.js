b61a1ba9b31703ef8de6a9b627248153fb728b84
George Pearson <george.pearson@dorsetsoftware.com>
2019-09-06 14:54:24 +0100

    Add footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter, faLinkedinIn, faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import * as style from './style.module.css';
 
 export default class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        Footer
+      <footer className={style.footer}>
+        <div className={style.socialIcons}>
+        <Link to="/">
+          <FontAwesomeIcon icon={faGithubAlt} className={style.socialIcon} size="3x"/>
+        </Link>
+        <Link to="/">
+          <FontAwesomeIcon icon={faLinkedinIn} className={style.socialIcon} size="3x"/>
+        </Link>
+        <Link to="/">
+          <FontAwesomeIcon icon={faTwitter} className={style.socialIcon} size="3x"/>
+        </Link>
+        <Link to="/">
+          <FontAwesomeIcon icon={faEnvelope} className={style.socialIcon} size="3x"/>
+        </Link>
+        </div>
+        <p>&copy; George Pearson {(new Date()).getFullYear()}</p>
       </footer>
     )
   }
