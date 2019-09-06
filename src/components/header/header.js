02396e2bebf83106c8d40b05d2e95e35626b4353
George Pearson <george.pearson@dorsetsoftware.com>
2019-09-06 10:19:03 +0100

    cleanup

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,44 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
-import * as style from './style.module.css';
-import { Link } from 'gatsby';
+import * as style from "./style.module.css";
+import { Link } from "gatsby";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default class Header extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      navIconActive: false
-    }
-  }
-
-  toggleNavIcon = () => {
-    this.setState({
-      navIconActive: !this.state.navIconActive,
-    });
-  }
-
   render() {
-    const navIcon = this.state.navIconActive ? faTimes : faBars;
-    const navBarMenuClass = this.state.navIconActive ? style.navBarMenuActive : style.navBarMenu;
     return (
       <header className={style.header}>
         <nav className={style.navBar}>
-          <div className={style.navBarBrand}>
+          <Link to="/">
             <img src={logo} className={style.logo} alt="logo" />
-            <div className={style.navIconDiv} onClick={this.toggleNavIcon}>
-              <FontAwesomeIcon className={style.navIcon} icon={navIcon} size="2x"/>
-            </div>
-          </div>
-          <div className={navBarMenuClass}>
+          </Link>
+          <button className={style.navButton} onClick={this.toggleNavIcon}>
+            <FontAwesomeIcon className={style.navButtonIcon} icon={faBars} size="2x"/>
+          </button>
+          <div className={style.navBarMenu}>
             <Link to="/" className={style.navBarItem}>Home</Link>
             <Link to="/" className={style.navBarItem}>About</Link>
             <Link to="/" className={style.navBarItem}>Photos</Link>
           </div>
         </nav>
-        
       </header>
     )
   }
