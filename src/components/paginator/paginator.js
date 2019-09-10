544ec55e28724239cef005e9fcc4bf7d91159ac0
George Pearson <george.pearson@dorsetsoftware.com>
2019-09-10 11:42:03 +0100

    style paginator

--- src/components/paginator/paginator.js
+++ src/components/paginator/paginator.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "gatsby";
+import * as style from "./style.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight  } from "@fortawesome/free-solid-svg-icons";
 
 export default class Paginator extends React.Component {
   
@@ -10,32 +13,33 @@ export default class Paginator extends React.Component {
     const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString();
     const nextPage = (currentPage + 1).toString();
     return (
-      <ul
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          listStyle: 'none',
-          padding: 0,
-        }}
-      >
-        {!isFirst && (
-          <Link to={prevPage} rel="prev">
-            ← Previous Page
-          </Link>
-        )}
-        {Array.from({ length: numPages }, (_, i) => (
-          <li key={`pagination-number${i + 1}`}>
-            <Link to={`/${i === 0 ? '' : i + 1}`}>{i + 1}</Link>
-          </li>
-        ))}
-        {!isLast && (
-          <Link to={nextPage} rel="next">
-            Next Page →
-          </Link>
-        )}
-      </ul>
+      <nav className={style.paginator}>
+        <ul className={style.paginatorList}>
+          {!isFirst && (
+            <li className={style.paginatorListItem}>
+              <Link className={style.paginatorLink} to={prevPage}>
+                  <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+              </Link>
+            </li>
+          )}
+          {Array.from({ length: numPages }, (_, i) => (
+            <li key={`pagination-number${i + 1}`}>
+              <Link 
+                className={i === (currentPage-1) ? style.paginatorLinkActive : style.paginatorLink}
+                to={`/${i === 0 ? '' : i + 1}`}>
+                {i + 1}
+              </Link>
+            </li>
+          ))}
+          {!isLast && (
+            <li className={style.paginatorListItem}>
+              <Link className={style.paginatorLink} to={nextPage}>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
     )
   }
 }
