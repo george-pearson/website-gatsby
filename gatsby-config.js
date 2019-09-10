cbe04b070563a83082e4e00fbe3851a35b8ef66c
George Pearson <george.pearson@dorsetsoftware.com>
2019-09-10 12:31:20 +0100

    Only include post files in GraphQL search

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `src`,
-        path: `${__dirname}/src`,
+        name: `posts`,
+        path: `${__dirname}/src/pages/posts`,
       },
     },
     {
