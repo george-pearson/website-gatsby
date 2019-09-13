b2f275ee130481c4b584abd5cda445679d852c91
George Pearson <george.pearson@dorsetsoftware.com>
2019-09-13 10:36:27 +0100

    sobel migration

---
+++ src/pages/posts/2019-05-19-sobel/flowerSobel/flowerSobel.js
@@ -0,0 +1,74 @@
+import React from "react";
+import flower from "../../../../../static/flower.png";
+import * as style from './style.module.css';
+import { sobel, greyscale } from '../sobel.js';
+
+export default class FlowerSobel extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.canvas = React.createRef();
+    this.image = React.createRef();
+  }
+
+  state = {
+    defaultFlowerImageHidden: false,
+    transformApplied: false
+  }
+  
+  render(){
+    return (
+    <div>
+      <div className={style.container}>
+        <canvas width="0" height="0" ref={this.canvas}></canvas>
+        <img 
+            ref={this.image}
+            className={
+            this.state.defaultFlowerImageHidden ? 
+            style.flowerImageHidden :
+            style.flowerImage
+            }
+            src={flower}
+            alt="flower"
+        />
+      </div>
+      <div>
+        <button onClick={this.applyClickHandler}>
+          {this.state.transformApplied ? "Reload" : "Apply"}
+        </button>
+      </div>
+    </div>
+    )
+  }
+
+  applyClickHandler = () => {
+    const canvas = this.canvas.current;
+    const ctx = canvas.getContext("2d");
+    const image = this.image.current;
+    canvas.width = image.width;
+    canvas.height = image.height;
+    ctx.drawImage(image, 0, 0);
+    if(!this.state.transformApplied){
+      const transformedImageData = this.applyTransform(canvas);
+      ctx.putImageData(transformedImageData, 0, 0);
+      this.setState({defaultFlowerImageHidden: true, transformApplied: true});
+    }
+    else{
+      this.setState({transformApplied: false});
+    }
+  }
+
+  applyTransform = (canvas) => {
+    switch(this.props.transformType){
+      case "greyscale":
+        return greyscale(canvas);
+      case "sobel":
+        return sobel(canvas, 0);
+      case "sobelColour":
+        return sobel(canvas, 1);
+      default:
+        const ctx = canvas.getContext("2d");
+        return ctx.getImageData(0, 0, canvas.width, canvas.height);
+    }
+  }
+}
---
+++ src/pages/posts/2019-05-19-sobel/sobelTool/sobelTool.js
@@ -0,0 +1,101 @@
+import React from "react";
+import * as style from "./style.module.css";
+import { sobel } from '../sobel.js';
+
+export default class SobelTool extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.canvas = React.createRef();
+    this.fileInput = React.createRef();
+    this.maxImageSize = 600;
+  }
+
+  state = {
+    imageSelected: false,
+    transformApplied: false,
+    saturation: 1
+  }
+  
+  render(){
+    return (
+    <div>
+      <canvas width="0" height="0" ref={this.canvas}></canvas>
+      <button onClick={this.selectImageButtonClickHandler}>Select an Image</button>
+      <input 
+        accept="image/*"
+        className={style.hidden}
+        type="file"
+        ref={this.fileInput}
+        onChange={this.selectImageChangeHandler}/>
+      {this.state.imageSelected && (
+        <div>
+          <div>
+          <label>Saturation:</label>
+          <input 
+            type="range"
+            step="0.01"
+            min="0"
+            max="1"
+            defaultValue={this.state.saturation}
+            onChange={this.saturationChangeHandler}/>
+          <span>{this.state.saturation}</span>
+          </div>
+          <button onClick={this.applyClickHandler}>
+            {this.state.transformApplied ? "Reload" : "Apply"}
+          </button>
+        </div>
+      )}
+    </div>
+    )
+  }
+
+  selectImageButtonClickHandler = () => {
+    this.fileInput.current.click();
+  }
+
+  selectImageChangeHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+        this.image = new Image();
+        this.image.onload = () => {
+          this.drawImageToScale(this.image, this.maxImageSize, this.canvas.current);
+        }
+        this.image.src = event.target.result;
+    }
+    const file  = e.target.files[0];
+    if (file && file.type.match('image.*')) {
+      reader.readAsDataURL(file);
+    }
+  }
+
+  saturationChangeHandler = (e) => {
+    this.setState({saturation: e.target.value});
+  }
+
+  applyClickHandler = () => {
+    const canvas = this.canvas.current;
+    const ctx = canvas.getContext("2d");
+    if(!this.state.transformApplied){
+      var id = sobel(canvas, this.state.saturation);
+      ctx.putImageData(id, 0, 0);
+      this.setState({transformApplied: true});
+    }
+    else{
+      this.drawImageToScale(this.image, this.maxImageSize, canvas);
+      this.setState({transformApplied: false});
+    }
+  }
+
+  drawImageToScale(image, maxSize, canvas){
+    let scale = 1;
+    if(maxSize < image.width || maxSize < image.height){
+      scale = Math.min(maxSize / image.width, maxSize / image.height);
+    }
+    canvas.width = image.width * scale;
+    canvas.height = image.height * scale;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    this.setState({imageSelected: true});
+  }
+}
