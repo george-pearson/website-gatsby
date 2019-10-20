b0eb0bd8eeb82bc663aa014f0f14237e1d0faf04
George Pearson <george-pearson@live.co.uk>
2019-10-20 20:58:01 +0100

    Add 404 page

---
+++ src/pages/404/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Layout from "../../components/layout/layout";
+import LogoBracketsNotFound from "../../../static/logoBracketsNotFound.inline.svg";
+import * as style from "./style.module.css";
+
+export default class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div className={style.container}>
+          <LogoBracketsNotFound className={style.logoBracketsNotFound}/>
+          <p>Uh-oh, we can't seem to find that page...</p>
+          <a className="blueLink" href="/">Back to homepage</a>
+        </div>
+      </Layout>
+    )
+  }
+}
