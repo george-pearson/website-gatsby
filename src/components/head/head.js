850471929e8bd93ce0d369e1e46d6e32a84fd91a
George Pearson <george-pearson@live.co.uk>
2019-10-27 17:22:27 +0000

    Add post images to SEO

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
 import { useSiteMetadata } from "../../hooks/useSiteMetadata";
+import logoImage from "../../../static/logo.png";
 
 export default (props) => {
     const { page, post } = props;
@@ -8,6 +9,7 @@ export default (props) => {
     const pageTitle = post ? post.frontmatter.title : page;
     const description = post ? post.frontmatter.subtitle : siteDescription;
     const title = `${pageTitle} | ${author}`;
+    const pageImage = post && post.frontmatter.image ? post.frontmatter.image : logoImage;
     return (
         <Helmet 
         title={title}
@@ -17,10 +19,12 @@ export default (props) => {
           { property: "og:site_name", content: siteUrl },
           { property: "og:description", content: description },
           { property: "og:title", content: title },
+          { property: "og:image", content: pageImage },
           { name: "twitter:title", content: title },
           { name: "twitter:creator", content: twitterUsername },
           { name: "twitter:card", content: "summary_large_image" },
-          { name: "twitter:description", content: description }
+          { name: "twitter:description", content: description },
+          { name: "twitter:image", content: pageImage },
         ]}/>
     )
 }
