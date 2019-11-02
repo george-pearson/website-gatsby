0c8d13d7db77001475169c305bf3c31b3cfbfb2c
George Pearson <george-pearson@live.co.uk>
2019-11-02 19:41:23 +0000

    Refactor head and add analytics

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -4,26 +4,38 @@ import { useSiteMetadata } from "../../hooks/useSiteMetadata";
 import logoImage from "../../../static/logo.png";
 
 export default ({page, post}) => {
-    const { author, siteDescription, siteUrl, twitterUsername } = useSiteMetadata();
-    const pageTitle = post ? post.frontmatter.title : page;
-    const description = post ? post.frontmatter.subtitle : siteDescription;
-    const title = `${pageTitle} | ${author}`;
-    const pageImage = post && post.frontmatter.image ? post.frontmatter.image : logoImage;
-    return (
-        <Helmet 
-        title={title}
-        meta={[
-          { name: "description", content: description },
-          { property: "og:url", content: siteUrl },
-          { property: "og:site_name", content: siteUrl },
-          { property: "og:description", content: description },
-          { property: "og:title", content: title },
-          { property: "og:image", content: pageImage },
-          { name: "twitter:title", content: title },
-          { name: "twitter:creator", content: twitterUsername },
-          { name: "twitter:card", content: "summary_large_image" },
-          { name: "twitter:description", content: description },
-          { name: "twitter:image", content: pageImage },
-        ]}/>
-    );
+  const { author, siteDescription, siteUrl, twitterUsername } = useSiteMetadata();
+  const pageTitle = post ? post.frontmatter.title : page;
+  const description = post ? post.frontmatter.subtitle : siteDescription;
+  const title = `${pageTitle} | ${author}`;
+  const pageImage = post && post.frontmatter.image ? post.frontmatter.image : logoImage;
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description}/>
+      <meta property="og:url" content={siteUrl}/>
+      <meta property="og:site_name" content={siteUrl}/>
+      <meta property="og:description" content={description}/>
+      <meta property="og:title" content={title}/>
+      <meta property="og:image" content={pageImage}/>
+      <meta name="twitter:title" content={title}/>
+      <meta name="twitter:creator" content={twitterUsername}/>
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:description" content={description}/>
+      <meta name="twitter:image" content={pageImage}/>
+      {process.env === "production" && (
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134616286-1" />
+      )}
+      {process.env === "production" && (
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-134616286-1');
+        `}
+      </script>
+      )}
+    </Helmet>
+  );
 };
