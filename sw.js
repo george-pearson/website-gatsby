6aa349163ae8cbdfa073c1f653700be433a917af
George Pearson <george-pearson@live.co.uk>
2020-04-01 21:27:17 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b0f2cf3f936a5442d42f.js"
+    "url": "webpack-runtime-833ab9d7d2b17ba07914.js"
   },
   {
     "url": "styles.94417129a3454f6452ff.css"
@@ -48,11 +48,11 @@ self.__precacheManifest = [
     "url": "795ab356-208aff79ac8ee20e87f0.js"
   },
   {
-    "url": "app-bfa6f7b308fcdb359827.js"
+    "url": "app-ef39b5c6aeac1c41ec5a.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "aa0d2e8ca7c9e500a8df8ea0cd188461"
+    "revision": "364fffb5c30a892eb49f4f6d0dd68c1b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-288479fdde1b1924312e.js"
@@ -63,7 +63,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "b9e9a2b7d7cfcb9b270387d019833e2b"
+    "revision": "477db51543bf4b63e7c6528874a37554"
   },
   {
     "url": "manifest.webmanifest",
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-bfa6f7b308fcdb359827.js`))) {
+  if (!resources || !(await caches.match(`/app-ef39b5c6aeac1c41ec5a.js`))) {
     return await fetch(event.request)
   }
 
